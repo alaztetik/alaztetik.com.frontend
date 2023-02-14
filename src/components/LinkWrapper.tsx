98672f25422815ac94253e2cb3e7626eb6b04301
Alaz Tetik <alaztetik@gmail.com>
2023-02-14 16:25:32 +0300

    fix typo

--- src/components/LinkWrapper.tsx
+++ src/components/LinkWrapper.tsx
@@ -3,7 +3,7 @@ import Link from "./Link";
 export default function LinkWrapper() {
   return (
     <div className="basis-1/3 flex flex-row justify-evenly lg:justify-start items-center grow flex-wrap min-w-[300px] p-2 gap-1">
-      <Link text="LinkedIn" href="https://linkedin.com/in/alaztetil" />
+      <Link text="LinkedIn" href="https://linkedin.com/in/alaztetik" />
       <Link text="Twitter" href="https://twitter.com/alaztetik" />
       <Link text="GitHub" href="https://github.com/alaztetik" />
       <Link text="Resume" href="https://registry.jsonresume.org/alaztetik" />
